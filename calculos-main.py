6b16504b2a40abac680efd00f82c14867b1c28f2
angelsantinoss <any.bello@duocuc.cl>
2026-06-10 21:14:57 -0400

    Add files via upload

---
+++ calculos-main.py
@@ -0,0 +1,22 @@
+calculos.py
+main.py
+
+        if opcion == "1":
+        apto = es_surfeable(altura_ola)
+        if apto:
+        print("Excelentes olas para el surf")
+        else:print("El mar esta muy tranquilo")
+        elif opcion == "4":
+        mostrar_advertencia_marejada()
+        elif opcion == "5":
+        altura_ola = float(input(("Ingresar la nueva altura de olas en metros: "))
+                           viento = int(input("Ingresa la nueva velocidad del viento en nudos: "))
+                           print("[INFO]: Datos actualizados")
+                           elif opcion == "6":
+                           print("Saliendo del sistema ....")
+                          break
+                          else:
+                          print("Opción no válida")
+                          if __name__== "__main__":
+                          menu()
+                          
---
+++ main.py
@@ -0,0 +1,25 @@
+while:#ideación conceptual
+      #Verificar si está vacio 
+      #Notificar el estado 
+      def: menu()
+      while time:print
+                 print
+                 print
+                 sin retorno
+                 beak:do-if("Red"
+                            in("Solarized-ligth")
+                            instance of:throw:void
+                            ("Solarized-Dark")
+while time:
+    with:while:#ideación conceptual
+      #Verificar si está vacio 
+      #Notificar el estado 
+      def: menu()
+      while time:print:                                                                                  
+      def mostrar_titulo():
+      print("\n=== MONITOREO DE OLAS===")
+      def titulo_menu():
+      return:if __name__ == "__main__":
+      elif opcion == "3":
+      temp_critica = obtener_temperatura_limite_traje()
+      
